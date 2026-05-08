f24b6cbbc571b8346eb55317c8b50e8acfc7aea8
NarekShabandari <narekshabandari01@gmail.com>
2026-05-08 20:20:19 +0330

    delete created

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -44,4 +44,13 @@ export class ProfilesService {
 
     return matchingProfile;
   }
+
+  remove(id: string): void {
+    const matchingProfileIndex = this.profiles.findIndex(
+      (profile) => profile.id === id,
+    );
+    if (matchingProfileIndex > -1) {
+      this.profiles.splice(matchingProfileIndex, 1);
+    }
+  }
 }
